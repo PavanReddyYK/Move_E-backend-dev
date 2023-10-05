48cdd30c0449e2dfe303475e6c1774e8c833278c
Pavan Reddy Y K <pavanykreddy24@gmail.com>
2023-10-05 11:59:59 +0530

    express server setup

---
+++ handler.mjs
@@ -0,0 +1,34 @@
+import express from 'express';
+const app = express()
+
+import http from 'http';
+
+import bodyParser from 'body-parser';
+app.use(bodyParser.urlencoded({"extended":true}))
+app.use(bodyParser.json())
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import cors from "cors";
+app.use(cors());
+
+
+
+
+
+app.use("*",(req,res,next)=>{
+    res.status(404).json({error:true,message:"Page Not Found!!!"})
+})
+
+app.use( (err,req,res,next)=>{
+    res.status(400).json({error:true,message:err.message,data:"ok"})
+} )
+
+
+app.set('port',process.env.REACT_APP_PORT)
+const server = http.createServer(app)
+
+server.listen(app.get('port'),"0.0.0.0",()=>{
+    console.log(`Express server listening on http://localhost:${app.get('port')}`)
+});
